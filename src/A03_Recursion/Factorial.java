449315d9334123adbb5a8e68c110ef73e36ce098
Sulemank-1 <suleman.dev42@gmail.com>
2026-09-15 18:52:01 +0500

    Study Section: The call stack with recursion

---
+++ src/A03_Recursion/Factorial.java
@@ -0,0 +1,15 @@
+package A03_Recursion;
+
+public class Factorial {
+    static void main(String[] args) {
+        System.out.println(fact(3));
+    }
+
+    public static int fact(int x){
+        if (x == 1)
+            return 1;
+        else
+            return x * fact(x-1);
+    }
+}
+
